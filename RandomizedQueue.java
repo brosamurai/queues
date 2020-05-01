b9b90b8495daed380a17424fa7784c9a5123b646
oarai <oarai@edu.uwaterloo.ca>
2020-05-01 15:11:14 -0400

    queue is working properly now

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -13,20 +13,7 @@ import java.util.NoSuchElementException;
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] q;
     private int n = 0;
-    private int head = 0;
     private int tail = 0;
-
-    /*
-    1. My dequeue function has a while loop that is used to find a random non-null
-    item to return. Same problem with the sample() function
-
-    2. Dequeue function has a flaw. When the 'head' gets detached,
-    I have to use a loop to find the next non-null element and make that the new head
-
-    3. Iterator is working but not to performance standards.
-    Need a way to return a random item but then also ensure that the same item
-    is not returned again
-     */
 
     // construct an empty randomized queue
     public RandomizedQueue() {
@@ -44,37 +31,38 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     // add the item
-    // what if every time you enqueue a new item, it gets swapped at random?
     public void enqueue(Item item) {
         if (item == null) {
             throw new IllegalArgumentException("Can't be doing that dawg...");
         }
-        // add item to tail + 1
         // for first addition, head = tail
         if (n == 0) {
-            head = 0;
             tail = 0;
-            q[head] = item;
+            q[0] = item;
         }
-        else if (tail == q.length - 1 && n < q.length) {
-            resize(q.length);
-            q[++tail] = item;
-        }
+
         else if (n < q.length) {
-            // can do ++tail
             q[++tail] = item;
+            swap();
         }
         else {
-            // we gotta resize the queue
             resize(2 * q.length);
             q[++tail] = item;
+            swap();
         }
         n++;
     }
 
+    private void swap() {
+        int randInt = StdRandom.uniform(tail + 1);
+        Item temp = q[randInt];
+        q[randInt] = q[tail];
+        q[tail] = temp;
+    }
+
     private void resize(int capacity) {
         Item[] copy = (Item[]) new Object[capacity];
-        int i = head;
+        int i = 0;
         int k = 0;
         while (i < tail + 1) {
             if (q[i] != null) {
@@ -84,42 +72,26 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             i++;
         }
         q = copy;
-        head = 0;
         tail = n - 1;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        // if (test) {
-        //     StdRandom.shuffle(q);
-        // }
         if (!isEmpty()) {
-            int rand = StdRandom.uniform(head, tail + 1);
-            while (q[rand] == null) {
-                rand = StdRandom.uniform(head, tail + 1);
-            }
-
-            // Will this be garbage collected? Is it even necessary?
+            // uniformly generate random number between 0 and tail
+            int rand = StdRandom.uniform(tail + 1);
             Item itemToReturn = q[rand];
 
             if (n == 1) {
-                head = 0;
                 tail = 0;
                 n--;
-                return q[head];
-            }
-            else if (rand == tail) {
-                tail--;
-            }
-            else if (rand == head) {
-                // TODO: big problem... how can we know where the new head must be?
-                // is this even necessary? yes
-                // what if we reset the array when this happens? ... not efficient
-                while (q[++head] == null) {
-                }
+                return q[0];
             }
 
-            q[rand] = null;
+            // put the item in tail in place of removed item
+            q[rand] = q[tail];
+            q[tail] = null;
+            tail--;
             n--;
             // q.length is always gonna be a power of 2
             if (n == q.length / 4) {
@@ -135,10 +107,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     // return a random item (but dont remove)
     public Item sample() {
         if (!isEmpty()) {
-            int rand = StdRandom.uniform(head, tail + 1);
-            while (q[rand] == null) {
-                rand = StdRandom.uniform(head, tail + 1);
-            }
+            int rand = StdRandom.uniform(tail + 1);
             return q[rand];
         }
         else {
@@ -151,18 +120,26 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         return new ListIterator();
     }
 
+    private Item[] shuffle() {
+        Item[] shuffledQ = (Item[]) new Object[n];
+        for (int i = 0; i < n; i++) {
+            // copy over items from q
+            shuffledQ[i] = q[i];
+            // random int between 0 and i
+            int r = StdRandom.uniform(i + 1);
+            Item temp = shuffledQ[i];
+            shuffledQ[i] = shuffledQ[r];
+            shuffledQ[r] = temp;
+        }
+        return shuffledQ;
+    }
+
     private class ListIterator implements Iterator<Item> {
-        private boolean shuffled = false;
-        private int numOfItemsProcessed = 0;
         private int i = 0;
         private boolean lastItem = false;
-        private Item[] copyOfq = q.clone();
+        private Item[] copyOfq = shuffle();
 
         public boolean hasNext() {
-            if (!shuffled) {
-                StdRandom.shuffle(copyOfq);
-                shuffled = true;
-            }
             return !lastItem;
         }
 
@@ -171,18 +148,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
 
         public Item next() {
-            if (numOfItemsProcessed == n) {
+            if (i == n) {
                 throw new NoSuchElementException("The randomized queue is empty my dawg");
             }
 
-            while (copyOfq[i] == null) {
-                i++;
-            }
             Item item = copyOfq[i];
-            numOfItemsProcessed++;
             i++;
 
-            if (numOfItemsProcessed == n) {
+            if (i == n) {
                 lastItem = true;
             }
 
@@ -191,23 +164,40 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public static void main(String[] args) {
-        // RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
-        // q.enqueue(1);
-        // q.enqueue(2);
-        // q.enqueue(3);
-        // q.enqueue(4);
-        // q.enqueue(5);
-        // q.enqueue(6);
-        // q.enqueue(7);
-        // q.dequeue();
-        // for (Integer i : q) {
-        //     System.out.println(i);
-        // }
-        // q.dequeue();
-        // q.dequeue();
-        // q.dequeue();
-        // q.dequeue();
-        // q.dequeue();
+        RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(6);
+        q.enqueue(7);
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+        q.dequeue();
+
+        // expect error here
+        try {
+            q.dequeue();
+        }
+        catch (NoSuchElementException ex) {
+            System.out.println("PASSED: threw exception cus queue is empty");
+        }
+
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(6);
+        q.enqueue(7);
+
+
+        // TEST FOR ITERATOR
         int n = 5;
         RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
         for (int i = 0; i < n; i++)
@@ -218,7 +208,5 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             StdOut.println();
         }
         System.out.println(queue);
-
-
     }
 }
